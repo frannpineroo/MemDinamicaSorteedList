5b8b6a504b3818e3671e49e9711ffcd6b4f89046
Francisco Piñero <frannpinero@hotmail.com>
2024-09-16 19:44:19 -0300

    Agregar archivos de proyecto.

---
+++ MemDinamicaSorteedList/Ejercicio77.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MemDinamicaSorteedList
+{
+    class Ejercicio77
+    {
+        static void Main() 
+        {
+            SortedList<string, string> diccio = new SortedList<string, string>();
+            diccio.Add("Hi", "Hola");
+            diccio.Add("How", "Cómo");
+            diccio.Add("are you?", "estás?");
+            string texto = "Hola,¿cómo estás?";
+            string[] partes = texto.Split(',');
+
+            foreach (string parte in partes)
+            {
+                Console.WriteLine(parte);
+            }
+
+
+            if (diccio.ContainsKey("adiós"))
+                Console.WriteLine(diccio["adiós"]);
+
+            Console.ReadKey();
+        }
+    }
+}
